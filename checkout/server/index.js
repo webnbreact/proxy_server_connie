d8e9a9c7c27070457b8a03053b101b7f25984ed9
Connie Ea <connieyeah@gmail.com>
2019-01-02 20:49:34 -0800

    proxy server setup

---
+++ checkout/server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const { getRoomBookingData } = require('./controllers.js');
+
+const app = express();
+
+const mainPagePath = path.join(__dirname, '/../public');
+app.use(express.static(mainPagePath));
+app.use('/rooms/:id/', express.static(mainPagePath));
+app.use(morgan('tiny'));
+
+app.get('/rooms/:id/booking', (req, res) => {
+  const roomId = req.params.id;
+  getRoomBookingData(roomId)
+    .then((roomInfo) => {
+      res.send(roomInfo);
+    }).catch((err) => {
+      console.error(err);
+      res.sendStatus(501);
+    });
+});
+
+const port = 1337;
+app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ proxy/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+var cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+  console.log(`server running on ${port}`)
+})
